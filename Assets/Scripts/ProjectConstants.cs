593f6cab53a1ec85fda9fe54f126b7c1fdf4d0fb
Mephilos <sth4628@hotmail.com>
2025-10-23 10:30:25 +0900

    플레이어 케릭터 히트 에니메이션 추가(장애물과 타격판정시)

--- Assets/Scripts/ProjectConstants.cs
+++ Assets/Scripts/ProjectConstants.cs
@@ -3,4 +3,5 @@ public static class ProjectConstants
 {
     public const string PLAYER_TAG = "Player";
     public const string OBSTACLE_TAG = "Obstacle";
+    public const string HIT_ANIM_PLAYER = "Hit";
 }
--- Assets/Scripts/_Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/_Player/PlayerCollisionHandler.cs
@@ -1,9 +1,37 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] float HitCooldownTime = 1f;
+    bool hitFlag = false;
+    
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        hitFlag = true;
+        StartCoroutine(HitCooldown());
+    }
     void OnCollisionEnter(Collision collision)
     {
+        HitTrigger();
         Debug.Log("hit");
+    }
+    void HitTrigger()
+    {
+        if (hitFlag == true) return;
+
+        if (hitFlag == false)
+        {
+            animator.SetTrigger(ProjectConstants.HIT_ANIM_PLAYER);
+            hitFlag = true;
+            StartCoroutine(HitCooldown());
+        }
+    }
+    IEnumerator HitCooldown()
+    {
+        yield return new WaitForSeconds(HitCooldownTime);
+        hitFlag = false;
     }
 }
